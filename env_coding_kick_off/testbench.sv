318e1cd033c7f15ca74ed107fd6b6f45770c3e18
Ashutosh-eda <mticd_202403@iiitm.ac.in>
2025-07-17 00:03:12 +0530

    Create testbench.sv

---
+++ env_coding_kick_off/testbench.sv
@@ -0,0 +1,43 @@
+// Code your testbench here
+// or browse Examples
+`include "cfs_algn_test_pkg.sv"
+module testbench();
+  
+  import uvm_pkg::*;
+  import cfs_algn_test_pkg::*;
+  
+  reg clk;
+ 
+  
+  initial begin
+    clk = 0;
+    forever begin
+      clk = #5ns ~clk;
+    end
+  end
+  
+  reg reset_n;
+  
+  initial begin
+    reset_n = 1;
+    #6ns;
+    reset_n = 0;
+    #30ns;
+    reset_n = 1;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+  
+  initial begin
+    run_test("");
+  end
+  
+  cfs_aligner dut(
+    .clk(    clk),
+    .reset_n(reset_n)
+  );
+  
+endmodule
